d01361b09b333387bbd0efc5311d2c6886c0db4c
Shane Barratt <stbarratt@gmail.com>
2015-10-12 18:43:34 -0700

    removed ipython for myles

---
+++ process_data.py
@@ -0,0 +1,24 @@
+import json
+# import IPython as ipy
+
+"""
+
+Natural 
+
+"""
+
+"""
+
+'name’: the name of the business
+'address’: the address of the business
+‘description’: a text description of the business
+‘website’: a list of URLs related to the business
+‘unique_id’: a unique identifier for this record
+
+"""
+
+with open('../challenge_set.json') as data_file:
+	res = json.load(data_file)
+		ipy.embed()
+
+print res
