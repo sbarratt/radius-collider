a514bfb9c33cf2aa0832bf132a251d6a720305f5
Shane Barratt <stbarratt@gmail.com>
2015-10-12 18:48:24 -0700

    fixed file

--- python/process_data1.py
+++ python/process_data1.py
@@ -1,12 +1,8 @@
 import json
 try:
 	import IPython as ipy
-
-"""
-Natural 
-"""
+except:
+	pass
 
 with open('../challenge_set.json') as data_file:
 	res = json.load(data_file)
-		ipy.embed()
-print res
