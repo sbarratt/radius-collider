e1cb8a01b56474ae8235b9d3ae33862d51ef7189
Shane Barratt <stbarratt@gmail.com>
2015-10-14 00:56:04 -0700

    updated file slightly

--- python/process_data.py
+++ python/process_data.py
@@ -1,8 +1,30 @@
-import json
+import json # For loading the data
+
+# For debugging purposes
 try:
 	import IPython as ipy
 except:
 	print "Couldn't import IPython"
 
+import nltk # Natural Language Processing Library
+import numpy as np # Numberical Python Library
+
+"""
+Keys: We must implement the whole thing in scala once we're done 
+(to maximize points, possibly put it in spark)
+
+NAICS (North America Industry Classification System)
+2 through 6-digit hierarchical classification system 
+*first 2 = economic sector
+*third = subsector
+*fourth = industry group
+*fifth = NAICS industry
+*sixth = national industry
+
+19256 NAICS codes
+"""
+
 with open('../challenge_set.json') as data_file:
 	res = json.load(data_file)
+
+ipy.embed()
--- python/process_data1.py
+++
@@ -1,8 +0,0 @@
-import json
-try:
-	import IPython as ipy
-except:
-	print "Couldn't import IPython"
-
-with open('../challenge_set.json') as data_file:
-	res = json.load(data_file)
