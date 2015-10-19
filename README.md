2c510763153585beba7050e240e37cb690a7ec06
Myles Scolnick <mscolnick@berkeley.edu>
2015-10-18 17:57:14 -0700

    updated business types

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Business NAICS Classifier
 
+The NAICS Classifier consists of an **assisted classifier** and a **database viewer** to see the scores assigned to each business, as well as a CLI to score each business and assign it a NAICS code.
+
+## Procedure
+
+#### Extra Data
+
+We first gathered some extra, public data. We got the latitude/longitude data for each business, the business type, and a title and description for each NAICS code.
+
+#### Scoring
+
+We created a way to score the chance of a business to be classified under a given NAICS code. These scores dont mean anything themselves, but are compared against other scores to determing which code is more probable. The scoring is based on similarirites between title and description of the business to the title and description of the NAICS code. More specifically, (TODO).
+
+We also took into account their business type. (TODO)
+
+
+#### Contextualizing
+
+Each businesses has its own list of probable NAICS codes each assigned a score, with a higher score representing a more probable chance that business is classified under that score. We also summed over all the scores by chopping then to their first 3 digits - this is the 3-code-buckets. This helps gives us a predition of which 3 code ubmrellas the business is under - this helps to remove outliers.
+
+#### Assigning a NAICS code
+
+The logic used to classify the business is take the max 6 digit code with the first 3 digits being the max of the 3-code-buckets.
+
 
 ## Running the server
 ```bash
--- python/scripts/get_business_types.py
+++ python/scripts/get_business_types.py
@@ -3,19 +3,16 @@ python/get_business_types.py
 
 Script to get businesses types from their lat lon
 """
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+import loader
+import requests
+import json
+import pickle
+from util import cosine_sim
 
-if __name__ == '__main__' and __package__ is None:
-  from os import sys, path
-  sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-  import loader
-  import requests
-  import json
-  import pickle
-  from util import cosine_sim
-  import IPython as ipy
-
-# API_KEY = "AIzaSyDEVQJmING4SQSZbcap0YYV6Dt4dFt78tY" #Myles
-API_KEY = 'AIzaSyCdVPTYLYttmFGw7wVxNewCFV-DFSuUcBw' #Shane - don't use it too much it will charge my credit card
+API_KEY = "AIzaSyDEVQJmING4SQSZbcap0YYV6Dt4dFt78tY" #Myles
+# API_KEY = 'AIzaSyCdVPTYLYttmFGw7wVxNewCFV-DFSuUcBw' #Shane - don't use it too much it will charge my credit card
 
 def url(code):
   return "http://api.naics.us/v0/q?year=2012&code={}".format(code)
@@ -24,12 +21,15 @@ def get_places(lat,lng):
   # uri = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&radius=500&key=%s' % (address, API_KEY)
   uri = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=50&key=%s' % (lat, lng, API_KEY)
   resp = json.loads(requests.get(uri).content)
+  if resp['status'] == 'OVER_QUERY_LIMIT':
+    raise Exception('over query limit')
   return resp['results']
 
 def get_all_business_types():
   businesses = loader.get_challengeset()
   idtoloc = loader.get_idtoloc()
   business_types_dict = loader.get_business_types()
+  print len(business_types_dict)
   for business in businesses:
     unique_id = business['unique_id']
     if unique_id not in business_types_dict.keys():
