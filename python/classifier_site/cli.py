d17f0f6edd2afc25d83d68adbd3974babd022b25
Myles Scolnick <mscolnick@berkeley.edu>
2015-10-21 13:26:46 -0700

    buzzwords

--- python/classifier_site/cli.py
+++ python/classifier_site/cli.py
@@ -12,6 +12,7 @@ import util
 import numpy as np
 from multiprocessing import Pool, cpu_count
 import IPython as ipy
+from sqlalchemy import or_
 import random
 import re
 
@@ -51,7 +52,7 @@ def ipyDebug():
 
 
 @manager.option('-c', '--chunk', dest='chunk', help='Specify a chunk [0, 1, 2]', required=True)
-@manager.option('-p', '--processes', dest='num_processes', help='Specify number of processes to use')
+@manager.option('-p', '--processes', dest='num_processes', help='Specify number of processes to use', required=False)
 def loadBusinesses(chunk, num_processes=cpu_count()):
     assert chunk in ['0', '1', '2'], "Chunk must be 0, 1, or 2"
     businesses = loader.get_challengeset(int(chunk))
@@ -138,66 +139,122 @@ def scoreLogic3(b, scores, threshold=.7):
         return ''
 
 
-@manager.command
-def classifyBusinessFast():
-  # WEIGHTS_DICT = {
-  #    'd_d_sim': .1,
-  #    't_t_sim': .2,
-  #    'd_t_sim': .1,
-  #    't_d_sim': .1,
-  #    't_t_w2vsim': 0.0,
-  #    'd_d_w2vsim': 0.0,
-  #    'd_t_w2vsim': 0.0,
-  #    't_d_w2vsim': 0.0
-  # }
-  for _ in range(100):
-    d_d_sim = random.random()
-    t_t_sim = 1.5
-    d_t_sim = random.random()
-    t_d_sim = 1.0
+@manager.option('-s', '--samples', dest='samples', help='Numbers of random weight samples', required=False)
+def classifyBusinesses(samples=1):
+    samples = int(samples)
     WEIGHTS_DICT = {
-       'd_d_sim': d_d_sim,
-       't_t_sim': t_t_sim,
-       'd_t_sim': d_t_sim,
-       't_d_sim': t_d_sim,
-       't_t_w2vsim': 0.0,
-       'd_d_w2vsim': 0.0,
-       'd_t_w2vsim': 0.0,
-       't_d_w2vsim': 0.0
+        'd_d_sim':  0.862052344506,
+        't_t_sim': 1.5,
+        'd_t_sim': 0.7694268978,
+        't_d_sim': 1.0,
+        't_t_w2vsim': 0.0,
+        'd_d_w2vsim': 0.0,
+        'd_t_w2vsim': 0.0,
+        't_d_w2vsim': 0.0
     }
-    index_to_id = loader.get_index_to_id()
-    id_to_bizid = loader.get_id_to_bizid()
-    S = loader.get_S()
+    THRESHOLD = .6 # [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]
 
-    w = []
-    for i,j in enumerate(['d_d_sim', 'd_d_w2vsim', 'd_t_sim', 'd_t_w2vsim', 't_d_sim', 't_d_w2vsim', 't_t_sim', 't_t_w2vsim']):
-      w.append(WEIGHTS_DICT[j])
-    S = [S[i]*w[i] for i in range(len(S))]
-    S = reduce(lambda x,y:x+y, S)
-    for i in range(10000):
-      ide = index_to_id[np.argmax(S[i,:])]
-      writeClassification(id_to_bizid[i], ide)
-    print d_d_sim, t_t_sim, d_t_sim, t_d_sim
-    predictionScoreOfTrainingSet()
+    # buzz words
+    ids_with_redbox = ids_for_query('redbox', ['name'])
+    ids_with_restaurant = ids_for_query('restaurant', ['name', 'business_type', 'description'])
+    ids_with_vet = ids_for_query('veterinary', ['name', 'business_type', 'description'])
+    ids_with_dentist = ids_for_query('dentist', ['name', 'business_type', 'description']) \
+        + ids_for_query('dental', ['name', 'business_type', 'description'])
+    ids_with_bank = ids_for_query('bank', ['business_type'])
+    ids_with_car_repair = ids_for_query('car%repair', ['name', 'business_type'])
+    ids_with_landscaping = ids_for_query('landscap', ['name', 'business_type'])
+    ids_with_locksmith = ids_for_query('locksmith', ['name', 'business_type'])
+    ids_with_hotel = ids_for_query('hotel', ['name', 'business_type']) \
+        + ids_for_query('motel', ['name', 'business_type'])
+    ids_with_photo = ids_for_query('photo', ['name', 'business_type'])
+
+    for threshold in xrange(samples):
+        # d_d_sim = random.random()
+        # t_t_sim = 1.5
+        # d_t_sim = random.random()
+        # t_d_sim = 1.0
+        # WEIGHTS_DICT = {
+        #    'd_d_sim': d_d_sim,
+        #    't_t_sim': t_t_sim,
+        #    'd_t_sim': d_t_sim,
+        #    't_d_sim': t_d_sim,
+        #    't_t_w2vsim': 0.0,
+        #    'd_d_w2vsim': 0.0,
+        #    'd_t_w2vsim': 0.0,
+        #    't_d_w2vsim': 0.0
+        # }
+        column_to_code = loader.get_index_to_id()
+        row_to_bizid = loader.get_id_to_bizid()
+        S = loader.get_S()
+
+        features_list = ['d_d_sim', 'd_d_w2vsim', 'd_t_sim', 'd_t_w2vsim', 't_d_sim', 't_d_w2vsim', 't_t_sim', 't_t_w2vsim']
+        w = [WEIGHTS_DICT[k] for k in features_list]
+        S = [Si*wi for Si, wi in zip(S, w)]
+        S = reduce(lambda x,y:x+y, S)
+        for i in xrange(10000):
+            bizid = row_to_bizid[i]
+            if bizid in ids_with_redbox:
+                code = 532230
+            elif bizid in ids_with_restaurant:
+                code = 72251
+            elif bizid in ids_with_vet:
+                code = 541940
+            elif bizid in ids_with_dentist:
+                code = 621210
+            elif bizid in ids_with_bank:
+                code = 52
+            elif bizid in ids_with_car_repair:
+                code = 811111
+            elif bizid in ids_with_landscaping:
+                code = 561730
+            elif bizid in ids_with_locksmith:
+                code = 561622
+            elif bizid in ids_with_hotel:
+                code = 721110
+            elif bizid in ids_with_photo:
+                code = 541921
+            else:
+                code = column_to_code[np.argmax(S[i,:])]
+                score = np.max(S[i,:])
+                if score > THRESHOLD:
+                    code = column_to_code[np.argmax(S[i,:])]
+                else:
+                    code = '' # no guess
+            writeClassification(row_to_bizid[i], code)
+        # print d_d_sim, t_t_sim, d_t_sim, t_d_sim
+        predictionScoreOfTrainingSet()
 
 
 @manager.command
 def predictionScoreOfTrainingSet():
     hand_classified_set = loader.get_hand_classifiedset()
     algo_classified_set = loader.get_algo_classifiedset()
-    score = 0
+    total = 0
+    scores = []
     for uid, actual in hand_classified_set.iteritems():
         guess = algo_classified_set[uid]
-        score += util.score_prediction(guess, actual)
-    total = len(hand_classified_set.keys()) * 6
-    print "Score: ", score
-    print "Total: ", total
-    print "%: ", score / float(total)
+        scores.append(util.score_prediction(guess, actual))
+    total = sum(scores)
+    max_potential = len(hand_classified_set.keys()) * 6
+    print "Score: ", total
+    print "Total: ", max_potential
+    print "%: ", total / float(max_potential)
+    unique, counts = np.unique(scores, return_counts=True)
+    print "Frequencies: \n", np.asarray((unique, counts)).T
 
 
 def writeClassification(business_uid, naics_code):
     loader.write_row_algo_classified_set(business_uid, naics_code)
 
+
+def ids_for_query(string, attributes):
+    regex = '%{}%'.format(string)
+    conditions = [getattr(Business, attr).ilike(regex) for attr in attributes]
+    condition = or_(*conditions)
+    ids = db.session.query(Business.unique_id).filter(condition).all()
+    if len(ids) == 0:
+        return []
+    return list(zip(*ids)[0])
 
 if __name__ == "__main__":
     naics_items = loader.get_naics_data_for_level(6)
